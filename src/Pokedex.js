deb8957feeb4994c6613aaf061f3660f870b1317
Anderson Alencar <anderson23alencar@gmail.com>
2021-10-09 13:14:11 -0300

    exibe pokemons por filtros

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -6,14 +6,30 @@ class Pokedex extends React.Component {
         super(props)
 
         this.state = {
-            index: 0
+            index: 0,
+            type: 'All',
         }
     }
 
+    listOfPokemons = () => {
+        const { index, type } = this.state;
+        const { pokemons } = this.props;
+        
+        if (type === 'All'){
+            return pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />).find((_pok, i) => i === index)
+        }
+        
+        return pokemons.filter((pok) => pok.type === type).map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />).find((_pok, i) => i === index)
+    }
+
     nextPokemon = () => {
-        const limit = this.props.pokemons.length - 1;
-    
-        if (this.state.index < limit) {
+        const { pokemons } = this.props;
+        const { type, index } = this.state;
+        let limit = pokemons.filter((pok) => pok.type === type).length - 1;
+
+        if (type === 'All') limit = pokemons.length - 1
+
+        if (index < limit) {
             this.setState((actualIndex, _props) => ({
                 index: actualIndex.index + 1
             }))
@@ -22,12 +38,28 @@ class Pokedex extends React.Component {
         }
     }
 
+    applyFilter = (event) => {
+        this.setState({ 
+            index: 0,
+            type: event.target.value
+        })
+    }
+
     render() {
+        
         return (
             <div className="pokedex">
-                {this.props.pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)
-                    .find((_pok, i) => i === this.state.index)}
-                <button onClick={this.nextPokemon}>Próximo</button>
+                <div>
+                    {
+                        this.listOfPokemons()
+                    }
+                    <button onClick={this.nextPokemon}>Próximo</button>
+                </div>
+                <div>
+                    <button onClick={this.applyFilter} value='All'>All</button>
+                    <button onClick={this.applyFilter} value='Fire'>Fire</button>
+                    <button onClick={this.applyFilter} value='Psychic'>Psychic</button>
+                </div>  
             </div>
         );
     }
